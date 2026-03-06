1141d1236597cf7bfedd38fc9fcc33d4a0669130
Karri Subhash <karrisubhash643@gmail.com>
2026-03-06 21:52:39 +0530

    Update settings.py

--- bus_tracker/settings.py
+++ bus_tracker/settings.py
@@ -183,7 +183,7 @@ SIMPLE_JWT = {
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_resis.core.RedisChannelLayer",
         "CONFIG":{
             "hosts":[os.environ.get("redis://red-d6le84f5r7bs73987r40:6379")],
     },
