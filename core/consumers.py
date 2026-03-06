27a2efcf91c37015c07f95ba6139b0462cb4f960
Karri Subhash <karrisubhash643@gmail.com>
2026-03-06 21:11:16 +0530

    Refactor BusLocationConsumer to use AsyncWebsocketConsumer

--- core/consumers.py
+++ core/consumers.py
@@ -1,6 +1,20 @@
-from django.urls import path
-from .consumers import BusLocationConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
-websocket_urlpatterns = [
-    path("ws/bus/", BusLocationConsumer.as_asgi()),
-]
+class BusLocationConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        await self.channel_layer.group_add(
+            "bus_locations",
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            "bus_locations",
+            self.channel_name
+        )
+
+    async def bus_location(self, event):
+        await self.send(text_data=json.dumps(event["data"]))
