433886f0b2aaead3ed4fc5d1e38df2e74bc4b9e2
Karri Subhash <karrisubhash643@gmail.com>
2026-03-10 00:13:00 +0530

    Update consumers.py

--- core/consumers.py
+++ core/consumers.py
@@ -1,4 +1,4 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+"""from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 class BusLocationConsumer(AsyncWebsocketConsumer):
@@ -18,3 +18,33 @@ class BusLocationConsumer(AsyncWebsocketConsumer):
 
     async def bus_location(self, event):
         await self.send(text_data=json.dumps(event["data"]))
+"""
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class BusLocationConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        await self.channel_layer.group_add(
+            "bus_locations",
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            "bus_locations",
+            self.channel_name
+        )
+
+    async def bus_location(self, event):
+
+        await self.send(text_data=json.dumps({
+            "trip_id": event["trip_id"],
+            "lat": event["lat"],
+            "lon": event["lon"],
+            "bus": event["bus"],
+            "driver": event["driver"],
+            "route": event["route"],
+            "has_issue": event["has_issue"]
+        }))
