3a69495bcaa553c8d3972303a1477f305194f4c8
Karri Subhash <karrisubhash643@gmail.com>
2026-03-13 16:15:19 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import timedelta
+from django.utils import timezone
 
 
 # ===================== USER =====================
@@ -88,10 +90,23 @@ class Trip(models.Model):
     def __str__(self):
         return f"Trip {self.id} — {self.bus} — {self.route.name} ({self.status})"
 
+class BusCurrentLocation(models.Model):
 
+    trip = models.OneToOneField(
+        "Trip",
+        on_delete=models.CASCADE
+    )
+
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.trip} - {self.lat},{self.lon}"
 
 # ===================== LOCATION PING =====================
-class LocationPing(models.Model):
+'''class LocationPing(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="pings")
     lat = models.FloatField()
     lon = models.FloatField()
@@ -107,3 +122,43 @@ class LocationPing(models.Model):
 
     def __str__(self):
         return f"Ping {self.id} — Trip {self.trip_id}"
+'''
+
+class LocationPingCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, trip_id):
+
+        # DELETE PINGS OLDER THAN 3 HOURS
+        cutoff = timezone.now() - timedelta(hours=3)
+        LocationPing.objects.filter(timestamp__lt=cutoff).delete()
+
+        trip = get_object_or_404(Trip, id=trip_id)
+
+        if request.user.role != "driver" or trip.driver != request.user:
+            return Response({"detail": "Not allowed"}, status=403)
+
+        data = request.data.copy()
+        data["trip"] = trip_id
+
+        serializer = LocationPingSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        ping = serializer.save(trip=trip)
+
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            "bus_locations",
+            {
+                "type": "bus_location",
+                "trip_id": trip.id,
+                "lat": ping.lat,
+                "lon": ping.lon,
+                "bus": trip.bus.registration_no,
+                "driver": trip.driver.username if trip.driver else "",
+                "route": trip.route.name if trip.route else "",
+                "has_issue": trip.has_issue
+            }
+        )
+
+        return Response(serializer.data, status=201)
