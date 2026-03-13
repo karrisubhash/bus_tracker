f94517ad414ff0eb27c35aa68f0d2ffab5f1790f
Karri Subhash <karrisubhash643@gmail.com>
2026-03-13 19:20:56 +0530

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Bus, Route, Trip, LocationPing
 
+
 @admin.action(description="Delete ALL location pings")
 def delete_all_pings(modeladmin, request, queryset):
     LocationPing.objects.all().delete()
+
 
 @admin.register(LocationPing)
 class LocationPingAdmin(admin.ModelAdmin):
     list_display = ("id", "trip", "lat", "lon", "timestamp")
     actions = [delete_all_pings]
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
@@ -21,7 +25,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'role', 'route', 'is_staff')
     list_filter = ('role', 'route')
 
+
 admin.site.register(Bus)
 admin.site.register(Route)
 admin.site.register(Trip)
-admin.site.register(LocationPing)
